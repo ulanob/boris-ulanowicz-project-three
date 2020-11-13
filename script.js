0dfdf4a1209075f445f45748d1f2b9ce57842827
Boris <ulanowicz_b@hotmail.com>
2020-11-12 19:47:02 -0800

    added basic CSS, added form on HTML, included basic JS and data structure

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-//        Project 3 Pseudocode: Choose your own adventure
+// create namespace
+const storyApp = {};
 
-//  Webpage welcomes the user to the scene of the adventure (... a castle?). There is a form to take the user's character name, along with a <select> element that drops down to give the user a "legendary title" (ie. Username the Cruel/Bold/Destroyer/Etc) and this string is saved to a variable on submit for later so that the app can refer to the user by name, providing a more personal/immersive experience. This form sits in a div that acts as the "platform" of where the story is told - basically a div with a border or a background image serving as a border.
+// create init function as a method of namespace object
+storyApp.init = function() {
+  console.log('ready');
+};
 
-// After the form submits (saves the username variable), the "story platform" (in this case, name entry form) clears, and an introductory scenario (<p>) is appended. At the end of the paragraph, the user is presented with two options <a> to progress in their adventure.
+// create an array "data" in the namespace to hold the story data. The array items will be objects that hold information such as the villains, the story text, and the backgrounds in an unnamed 
+storyApp.dataArray = [
+  {
+    villain: 'warlock',
+    storyText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt delectus quia quas repellendus voluptates hic nobis. Sunt adipisci obcaecati nemo sequi, illo sint velit mollitia impedit aperiam rem error cumque soluta voluptatibus eum debitis id deleniti repellat deserunt et sit!'
+  },
+  {
+    friend: 'warlock',
+    storyText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt delectus quia quas repellendus voluptates hic nobis.Sunt adipisci obcaecati nemo sequi, illo sint velit mollitia impedit aperiam rem error cumque soluta voluptatibus eum debitis id deleniti repellat deserunt et sit!'
+  },
+  {
+    villain: 'dragon',
+    storyText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt delectus quia quas repellendus voluptates hic nobis. Sunt adipisci obcaecati nemo sequi, illo sint velit mollitia impedit aperiam rem error cumque soluta voluptatibus eum debitis id deleniti repellat deserunt et sit!'
+  },
+  {
+    friend: 'dragon',
+    storyText: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt delectus quia quas repellendus voluptates hic nobis.Sunt adipisci obcaecati nemo sequi, illo sint velit mollitia impedit aperiam rem error cumque soluta voluptatibus eum debitis id deleniti repellat deserunt et sit!'
+  }
+]
 
-// The <a> tag, on click, then clears the <p> tag of story platform <div> and appends a new scenario that builds around the negative/positive consequences of the user's choice. Two further <a> tags are presented for the user to choose from, along with a button up top that allows the user to return to the previous scenario.
 
-// Similar to before, the two choices lead the user to a unique end of story- there will be 4 different endings to the adventure. The usual <a> tags are different at the end here: one is made invisible, and the other brings the user back to the top where they can restart their adventure. The "return" button is also present so that the user can go back one step and try the other option.  
+// first function: get userName
+storyApp.getUsername = function() {
+  // 
+}
+// create an event listener on form input to take the name
+// prevent default on input
+// create variable userName to save the user's name
+// create an event listener on the 
 
-// The indiviual <p> tags can be written in the JS file in an object instead of in the HTML 
-
-// Stretch goals: on a selection, two random numbers are generated - one for the user "skill" and the second number for minimum requirement". If user skill > requirement, then the user proceeds to selected outcome. If fail, then the user is "punished" to an alternate outcome. User skill integer can be incorporated into the next scene's content, or perhaps onto the clicked on <a> tag.
+// document ready with init method
+$(function() {
+  storyApp.init();
+})
